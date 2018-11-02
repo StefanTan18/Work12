5ee873279343e4695248311d4caaf939b0f3cadb
Stefan Tan <stan3@stuy.edu>
2018-11-01 23:26:04 -0400

    updated dirinfo.c

--- dirinfo.c
+++ dirinfo.c
@@ -6,16 +6,28 @@
 
 int main() {
   DIR * d;
-  d = opendir("../Work12");
-  struct dirent * entry;
+  d = opendir(".");
+  struct dirent * entry = readdir(d);
+  struct stat meta;
+  int total = 0;
+ 
+  while (entry) {
+    stat(entry->d_name, &meta);
+    total += meta.st_size;
+    entry = readdir(d);
+  }
+
+  printf("Total Directory Size: %d Bytes\n", total);
+
+  /*
   if(readdir(d)){
     printf("NO D\n");
     return 0;
-  }
+    }
   while(readdir(d)){
     entry = readdir(d);
     printf("%s", entry->d_name);
-  }
+    }*/
   closedir(d);
   return 0;
 }
